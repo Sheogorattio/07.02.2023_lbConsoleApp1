f47a594aba76b8d605039c9724de9a4a31e70351
Sheogorattio <no.passion.there.is.serenity@gmail.com>
2023-02-09 01:36:21 +0200

    Add project files.

---
+++ 07.02.2023_lbConsoleApp1/City.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CitySpace
+{
+    public class City
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public int Id { get; set; }
+        public int Code { get; set; }
+        public int Population { get; set; }
+
+        private string[] Districts = new string[0];
+
+        public void AddDistrict(string name)
+        {
+            string[] _districts = new string[Districts.Length+1];
+            for(int i = 0; i < Districts.Length; i++)
+            {
+                _districts[i] = Districts[i];
+            }
+            _districts[_districts.Length-1] = name;
+            Districts = new string[_districts.Length];
+            for(int i=0; i < Districts.Length; i++)
+            {
+                Districts[i] = _districts[i];
+            }
+        }
+
+        public void PrintDistricts()
+        {
+            foreach(var district in Districts)
+            {
+                if(district == null) continue;
+                Console.WriteLine(district);
+            }
+        }
+
+        public string GetDistrict(int index)
+        {
+            return Districts[index];
+        }
+
+        public void DeleteDistrict(int index)
+        {
+            Districts[index] = null;
+        }
+    }
+}
---
+++ 07.02.2023_lbConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitySpace;
+
+namespace _07._02._2023_lbConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            City city = new City();
+            city.AddDistrict("malynovskiy");
+            city.AddDistrict("prymorskiy");
+            city.AddDistrict("kyivskiy");
+            city.PrintDistricts();
+            city.DeleteDistrict(1);
+            city.PrintDistricts();
+            Console.ReadKey();
+        }
+    }
+}
